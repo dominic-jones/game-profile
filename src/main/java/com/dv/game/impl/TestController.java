5f06d45da5957d6e72787f85124a3c4f98eee472
Dominic Jones <dominic.jones@elasticpath.com>
2013-11-23 12:29:11 +0000

    Displaying username on login

--- src/main/java/com/dv/game/impl/TestController.java
+++ src/main/java/com/dv/game/impl/TestController.java
@@ -2,10 +2,12 @@ package com.dv.game.impl;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.security.Principal;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -17,7 +19,10 @@ public class TestController {
     EntityManager em;
 
     @RequestMapping(value = "/a", method = GET)
-    public String test() {
+    public String test(Principal principal,
+                       Model model) {
+
+        model.addAttribute("username", principal.getName());
 
         return "test";
     }
