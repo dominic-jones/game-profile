97f5bf75102ff9cf0dae6abbeef9191d9d3d27ff
Dominic Jones <dominic.jones@elasticpath.com>
2013-11-25 17:07:40 +0000

    Expanding database bootstrap

--- src/main/java/com/dv/game/impl/DataBootstrap.java
+++ src/main/java/com/dv/game/impl/DataBootstrap.java
@@ -18,10 +18,25 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
     @PersistenceContext
     private EntityManager em;
 
+    /*
+     * Temporary data bootstrapping.
+     *
+     * This needs to be an ApplicationListener<ContextRefreshedEvent> so that all beans are loaded and initialised before
+     * this executes, to take advantage of transaction support etc.
+     *
+     * An @PostConstruct method would be preferred, but it seems at that time it runs, all of Spring is not yet loaded.
+     */
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
         Iterable<GrantedAuthority> roles = Lists.<GrantedAuthority>newArrayList(new SimpleGrantedAuthority("ROLE_USER"));
-        em.persist(new User("user", "password", roles));
+        User user = new User("user", "password", roles);
+        System.out.println("@@@: " + user.getId());
+        em.persist(user);
+
+        roles = Lists.<GrantedAuthority>newArrayList(new SimpleGrantedAuthority("ROLE_USER"), new SimpleGrantedAuthority("ROLE_ADMIN"));
+        user = new User("admin", "password", roles);
+        System.out.println("@@@: " + user.getId());
+        em.persist(user);
     }
 }
