8b8898cf0d2966797d3c8d9341f31dd414e512b8
Dominic Jones <dominic.jones@elasticpath.com>
2013-12-04 11:09:05 +0000

    Removing bean validation from the login model. For now it is unused.

--- src/main/java/com/dv/game/login/LoginEditModel.java
+++ src/main/java/com/dv/game/login/LoginEditModel.java
@@ -1,20 +1,9 @@
 package com.dv.game.login;
 
-import org.hibernate.validator.constraints.Length;
-
-import javax.validation.constraints.NotNull;
-
-//TODO 2013-11-27 Dom - Probably do not want to do the same bean validation as we do on registration.
 public class LoginEditModel {
 
-    @NotNull
-    //TODO 2013-11-27 Dom - Length is temporary, only here to spike bean validation
-    @Length(min = 4, max = 10)
     public String username;
 
-    @NotNull
-    //TODO 2013-11-27 Dom - Length is temporary, only here to spike bean validation
-    @Length(min = 8)
     private String password;
 
     public String getUsername() {
